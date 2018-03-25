0238d188c0dc4f275f4e2e015a0fcb2859308398
Hendi Santika <hendisantika@gmail.com>
2018-03-26 04:11:09 +0700

    #4 - Add Mail Domain

---
+++ src/main/java/com/hendisantika/simplemail/mail/Mail.java
@@ -0,0 +1,70 @@
+package com.hendisantika.simplemail.mail;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-mail
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/03/18
+ * Time: 04.10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mail {
+    private String from;
+    private String to;
+    private String subject;
+    private String content;
+
+    public Mail() {
+    }
+
+    public Mail(String from, String to, String subject, String content) {
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Mail{" +
+                "from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", subject='" + subject + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
